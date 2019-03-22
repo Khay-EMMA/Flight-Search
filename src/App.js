d380390e09b4f227372ec10ca99ea1cb6cefa612
AdityaHirapara <adityahirapara2016@gmail.com>
2019-03-22 22:06:47 +0530

    :electric_plug: Setup redux store :card_file_box:

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Flight Search
+        <Header/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const render = () => {
+  return ReactDOM.render(<App />, document.getElementById("root"));
+};
 
+render();
+store.subscribe(render);
 serviceWorker.register();
